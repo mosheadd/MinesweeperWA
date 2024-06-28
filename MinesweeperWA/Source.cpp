338bb410242f7062b6cac5460a02f8cbac870066
mosheadd <rost1234567890qgp@gmail.com>
2024-06-28 13:00:45 +0300

    Добавьте файлы проекта.

---
+++ MinesweeperWA/Source.cpp
@@ -0,0 +1,64 @@
+#include <Windows.h>
+#include <Winuser.h>
+
+#include "MainClassPrototypes.h"
+
+enum MainWindowParams {
+	X = 630,
+	Y = 300,
+	HEIGHT = 500,
+	WIDTH = 500
+};
+
+int WINAPI WinMain(HINSTANCE hInst, HINSTANCE hPrevInst, LPSTR args, int ncmdshow)
+{
+
+	WNDCLASS MainClass = CreateWindowClass((HBRUSH)COLOR_WINDOW, LoadCursor(NULL, IDC_ARROW), hInst, LoadIcon(hInst, IDI_EXCLAMATION),
+		L"MainWindow", MainClassProcedure);
+
+	if (!RegisterClass(&MainClass)) return -1;
+
+	MSG MainMessage = { 0 };
+
+	CreateWindow(MainClass.lpszClassName, L"Window", WS_OVERLAPPED | WS_CAPTION | WS_SYSMENU | WS_MINIMIZEBOX | WS_VISIBLE, X, Y, WIDTH, HEIGHT, NULL,
+		NULL, NULL, NULL);
+
+	while (GetMessage(&MainMessage, NULL, NULL, NULL))
+	{
+		TranslateMessage(&MainMessage);
+		DispatchMessageW(&MainMessage);
+	}
+
+	return 0;
+}
+
+WNDCLASS CreateWindowClass(HBRUSH color, HCURSOR cursor, HINSTANCE hInst, HICON icon, LPCWSTR name, WNDPROC procedure)
+{
+
+	WNDCLASS wndclass = { 0 };
+
+	wndclass.hCursor = cursor;
+	wndclass.hIcon = icon;
+	wndclass.lpszClassName = name;
+	wndclass.hInstance = hInst;
+	wndclass.hbrBackground = color;
+	wndclass.lpfnWndProc = procedure;
+
+	return wndclass;
+}
+
+LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam)
+{
+
+	switch (msg)
+	{
+	case WM_CREATE:
+		break;
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+	default:
+		return DefWindowProc(hwnd, msg, wparam, lparam);
+	}
+
+}
