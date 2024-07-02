0f2270c851df8dbf8ec299e4662853816bf4eeb8
mosheadd <rost1234567890qgp@gmail.com>
2024-07-02 17:16:45 +0300

    Readd menu and make Start button working

--- MinesweeperWA/Source.cpp
+++ MinesweeperWA/Source.cpp
@@ -81,6 +81,7 @@ LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM l
 		case Start:
 			hasGameStarted = true;
 			HideMainMenuWidgets();
+			InvalidateRect(hwnd, NULL, FALSE);
 			break;
 		case Exit:
 			result = MessageBox(hwnd, L"Are you sure you want to exit the game?", L"Exit", MB_YESNO);
@@ -91,77 +92,83 @@ LRESULT CALLBACK MainClassProcedure(HWND hwnd, UINT msg, WPARAM wparam, LPARAM l
 	case WM_CREATE:
 		AddMainWindowMenu(hwnd);
 		AddMainWindowWidgets(hwnd);
-		//ShowMainMenuWidgets();
+		ShowMainMenuWidgets();
 		break;
 	case WM_PAINT:
 		hdc = BeginPaint(hwnd, &ps);
 
-		for (int i = 0; i < mapHeight; i++)
+
+		if (hasGameStarted)
 		{
-			for (int j = 0; j < mapWidth; j++)
+
+			for (int i = 0; i < mapHeight; i++)
 			{
-				short cell = map.getCell(i, j);
-
-				short minescount = 0;
-				std::wstring wstrMinescount;
-
-				switch(cell)
+				for (int j = 0; j < mapWidth; j++)
 				{
-				case mswa::Map::UNCOVERED:
-					minescount = map.getMinesCount(i, j);
-					if (!minescount)
-					{
-						drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\uncovered.jpg", hdc, j * 32, i * 32);
-					}
-					else
-					{
-						wstrMinescount = L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\" + std::to_wstring(minescount) + L".jpg";
-						drawImage(wstrMinescount.c_str(), hdc, j * 32, i * 32);
-					}
-					break;
-				case mswa::Map::FLAGGED:
-					drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\flag.jpg", hdc, j * 32, i * 32);
-					break;
-				case mswa::Map::FLAGGED_MINE:
-					drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\flag.jpg", hdc, j * 32, i * 32);
-					break;
-				default:
-					drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\covered.jpg", hdc, j * 32, i * 32);
-				};
+					short cell = map.getCell(i, j);
 
+					short minescount = 0;
+					std::wstring wstrMinescount;
+
+					switch (cell)
+					{
+					case mswa::Map::UNCOVERED:
+						minescount = map.getMinesCount(i, j);
+						if (!minescount)
+						{
+							drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\uncovered.jpg", hdc, j * 32, i * 32);
+						}
+						else
+						{
+							wstrMinescount = L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\" + std::to_wstring(minescount) + L".jpg";
+							drawImage(wstrMinescount.c_str(), hdc, j * 32, i * 32);
+						}
+						break;
+					case mswa::Map::FLAGGED:
+						drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\flag.jpg", hdc, j * 32, i * 32);
+						break;
+					case mswa::Map::FLAGGED_MINE:
+						drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\flag.jpg", hdc, j * 32, i * 32);
+						break;
+					default:
+						drawImage(L"C:\\Users\\rost1\\source\\repos\\MinesweeperWA\\images\\covered.jpg", hdc, j * 32, i * 32);
+					};
+
+				}
 			}
 		}
 		EndPaint(hwnd, &ps);
 		break;
 	case WM_LBUTTONUP:
-
-		GetCursorPos(&p);
-		if (ScreenToClient(hwnd, &p))
+		if (hasGameStarted)
 		{
-
-			if (!firstMoveDone && p.x <= 32 * mapWidth && p.y <= 32 * mapHeight)
+			GetCursorPos(&p);
+			if (ScreenToClient(hwnd, &p))
 			{
-				firstMoveDone = true;
-				map.initMines(p.x, p.y);
+
+				if (!firstMoveDone && p.x <= 32 * mapWidth && p.y <= 32 * mapHeight)
+				{
+					firstMoveDone = true;
+					map.initMines(p.x, p.y);
+				}
+
+				if (!map.action(p.x, p.y))
+				{
+					MessageBox(hwnd, L"You lost!", L"Game lost", MB_ICONEXCLAMATION | MB_OK);
+					PostQuitMessage(0);
+				}
+
+
+				if (map.checkWinCondition())
+				{
+					MessageBox(hwnd, L"You won!", L"Game won", MB_ICONEXCLAMATION | MB_OK);
+					PostQuitMessage(0);
+				}
+
+				InvalidateRect(hwnd, NULL, FALSE);
+
 			}
-
-			if (!map.action(p.x, p.y))
-			{
-				MessageBox(hwnd, L"You lost!", L"Game lost", MB_ICONEXCLAMATION | MB_OK);
-				PostQuitMessage(0);
-			}
-
-
-			if (map.checkWinCondition())
-			{
-				MessageBox(hwnd, L"You won!", L"Game won", MB_ICONEXCLAMATION | MB_OK);
-				PostQuitMessage(0);
-			}
-
-			InvalidateRect(hwnd, NULL, FALSE);
-
 		}
-
 		break;
 	case WM_RBUTTONUP:
 		if (!firstMoveDone) break;
@@ -196,7 +203,7 @@ void AddMainWindowWidgets(HWND hwnd)
 	StartGame = CreateWindowA("button", "Start", WS_CHILD, WIDTH * 3 / 8, HEIGHT / 4, WIDTH / 4, HEIGHT / 8, hwnd, (HMENU)Start, NULL, NULL);
 	GameSettings = CreateWindowA("button", "Settings", WS_CHILD, WIDTH * 3 / 8, 3 * HEIGHT / 8, WIDTH / 4, HEIGHT / 8, hwnd, (HMENU)Settings, NULL, NULL);
 	ExitGame = CreateWindowA("button", "Exit", WS_CHILD, WIDTH * 3 / 8, HEIGHT / 2, WIDTH / 4, HEIGHT / 8, hwnd, (HMENU)Exit, NULL, NULL);
-	forTests = CreateWindowA("static", "a", WS_CHILD | WS_VISIBLE, 330, 330, 200, 200, hwnd, NULL, NULL, NULL);
+	forTests = CreateWindowA("static", "", WS_CHILD | WS_VISIBLE, 330, 330, 200, 200, hwnd, NULL, NULL, NULL);
 
 }
 
